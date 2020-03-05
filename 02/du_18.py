06972a6871398644cf514ea0eb767c6b326e9bfe
Michaela Hlacikova <m.hlacikova@gmail.com>
2020-03-05 11:05:14 +0100

    Pyladies lesson

---
+++ 02/du_18.py
@@ -0,0 +1,22 @@
+# Tento program rozdává naivní rady do života.
+
+print('Odpovídej "ano" nebo "ne".')
+stastna_retezec = input('Jsi šťastná? ')
+bohata_retezec = input('Jsi bohatá? ')
+
+if stastna_retezec == 'ano':
+    if bohata_retezec == 'ano':
+        print('Gratuluji!')
+    elif bohata_retezec == 'ne':
+        print('Zkus míň utrácet.')
+    else:
+        print('Nerozumím!')
+elif stastna_retezec == 'ne':
+    if bohata_retezec == 'ano':
+        print('Zkus se víc usmívat.')
+    elif bohata_retezec == 'ne':
+        print('To je mi líto.')
+    else:
+        print('Nerozumím!')
+else:
+    print('To je mi líto.')
