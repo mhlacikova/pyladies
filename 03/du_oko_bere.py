1fb7480633f70a02716fe16ffbca0ff827587a1b
Michaela Hlacikova <m.hlacikova@gmail.com>
2020-03-10 18:54:00 +0100

    Pyladies lesson

---
+++ 03/du_oko_bere.py
@@ -0,0 +1,32 @@
+from random import randrange
+print('Vitam ta v hre Oko bere! \nCielom hry je ziskat co najviac bodov, maximalne vsak 21. \nV pripade, ze ziskas viac bodov, prehravas. \nTahaj a neboj sa riskovat!')
+
+body = 0
+print('Tvoj aktualny pocet bodov je:',body)
+vstup = input('Chces potiahnut kartu? Napis ano/nie: ')
+
+while vstup == 'ano':
+    body = body + randrange(2,10)
+    print('Tvoj aktualny pocet bodov je:',body)
+    if body > 21:
+        print('Prehral si!')
+        break
+    elif body < 21:
+        print('Este skus tahat!')
+        vstup = input('Chces potiahnut kartu? Napis ano/nie: ')
+        if vstup == 'nie':
+            break
+    else:
+        print('Gratulujem! Vyhral si!')
+        break
+
+if vstup == 'nie':
+    print('Tvoj aktualny pocet bodov je:',body)
+    if body < 21:
+        print('Mohol si sa viac snazit a riskovat!')
+
+elif not (vstup == 'ano') or (vstup == 'nie'):
+     print('Nerozumiem')
+
+else:
+    print('Dakujem za hru.')
